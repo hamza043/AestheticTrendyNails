730d6fa29597a633f89fc41acc2b936070949f8c
hamza043 <muhamadhamza474@gmail.com>
2024-09-28 22:27:34 +0500

    frontend completed

---
+++ frontend/src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './DescriptionBox.css';
+
+
+const DescriptionBox = () => {
+    return (
+        <div className="descriptionbox">
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>AestheticTrendyNails is a one-stop online store for premium, stylish, and trendy nail sets that cater to fashion-forward individuals. Our carefully curated collection features an array of unique, elegant nail designs that are both affordable and of the highest quality. Whether you're looking for glittery, vibrant, or minimalist designs, AestheticTrendyNails has something for everyone. Our mission is to provide customers with nails that add the perfect finishing touch to any look, helping them express their personality through bold and creative designs. Each set is designed for comfort, durability, and long-lasting wear, making it easy to get salon-quality nails from the comfort of your home. Explore our collection and redefine your style with AestheticTrendyNails today!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, corrupti, iste nobis laborum ducimus nihil laboriosam, pariatur illum magni quidem eius cupiditate aliquam odit doloribus in aut praesentium. Natus, ex!</p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox;
